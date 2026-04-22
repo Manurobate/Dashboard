036b1829766003fdc8aa95c3d6c7d653b2144412
Manurobate <contact@robate.fr>
2026-04-22 23:21:14 +0200

    Déploiement Docker Compose avec MariaDB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { validationSchema } from './config/app.config';
@@ -9,6 +10,21 @@ import { validationSchema } from './config/app.config';
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mariadb',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+        synchronize: false,
+        logging: config.get<string>('NODE_ENV') === 'development',
+      }),
     }),
   ],
   controllers: [AppController],
